d8962aeded2735ceb4c990fe5d63ce44a98c9606
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2024-01-21 14:35:20 -0300

    (back) chore: add drf spectacular schema

--- backend/inseproject/inseproject/settings.py
+++ backend/inseproject/inseproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inseapp',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema'
+}
+
+SPETACULAR_SETTINGS = {
+    'Title':'INSE',
+     
+}
